7eeda245adc2ae413844e40c791a8abdb08de2dd
Cloud Jung <cloud.jung@gmail.com>
2025-06-08 18:25:20 +0900

    init auth project

--- other/nextnews/app/news/[slug]/page.js
+++ other/nextnews/app/news/[slug]/page.js
@@ -2,21 +2,30 @@ import { DUMMY_NEWS } from "@/dummy-news";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
+import classes from "./page.module.css";
+
 export default async function NewsDetailPage({ params }) {
-  const newsSlug = await params.slug;
+	const newsSlug = await params.slug;
 	const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsSlug);
 
-	if(!newsItem) {
+	if (!newsItem) {
 		notFound();
 	}
 
 	return (
 		<article className="news-article">
 			<header>
-				<Image src={`/images/news/${newsItem.image}`} alt={newsItem.title} width={700} height={200}/>
-				<h1>{newsItem.title}</h1>
-				<time dateTime={newsItem.date}>{newsItem.date}</time>
-			</header>	
+				<div className={classes.image}>
+					<h1>{newsItem.title}</h1>
+					<time dateTime={newsItem.date}>{newsItem.date}</time>
+
+					<Image
+						src={`/images/news/${newsItem.image}`}
+						alt={newsItem.title}
+						fill
+					/>
+				</div>
+			</header>
 			<p>{newsItem.content}</p>
 		</article>
 	);
